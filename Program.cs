11313427c91d09cc40389d01fc81a7add1af4158
Hasannp0611 <hasannp0611@gmail.com>
2024-03-18 05:10:19 +0700

    Menambahkan Class HaloGenetic dengan method SapaUser

--- Program.cs
+++ Program.cs
@@ -1,3 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+public class HaloGeneric
+{
+    public String SapaUser(String X) {
+        return "Halo " + X;
+    }
+
+
+}
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        HaloGeneric sapaan = new HaloGeneric();
+        String nama = "Hasan Nurrahman";
+        String sapa = sapaan.SapaUser(nama);
+        Console.WriteLine(sapa);
+    }
+}
 
