6ce4add2ee44a09ad192ea52cf31cca139f918e8
Hasannp0611 <hasannp0611@gmail.com>
2024-03-18 05:23:32 +0700

    Menambahkan class DataGeneric

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+public class DataGeneric<D>
+{
+    private D data;
+    public DataGeneric(D NIM)
+    {
+        this.data = NIM;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine("Data yang tersimpan adalah NIM: " + this.data);
+    }
+}
